0f357fd5de7819b33983deefb8f39a022efae374
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-01-15 18:26:30 +0100

    code refactoring

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -48,7 +48,51 @@ fun LemonadeApp() {
     var task: LemonScreens by remember {
         mutableStateOf(LemonScreens.SELECT)
     }
-    val textResource: Int = when (task) {
+    var squeezes: Int = (2..4).random()
+    var countSqueezes by remember {
+        mutableStateOf(0)
+    }
+    val resetSqueezes = {
+        squeezes = (2..4).random()
+        countSqueezes = 0
+    }
+    when (task) {
+        LemonScreens.SELECT -> LemonScreen(
+            textResource = R.string.select_lemon,
+            imageResource = R.drawable.lemon_tree,
+            imageDescription = R.string.lemon_tree
+        ) {
+            task = LemonScreens.SQUEEZE
+        }
+        LemonScreens.SQUEEZE -> LemonScreen(
+            textResource = R.string.squeeze_lemon,
+            imageResource = R.drawable.lemon_squeeze,
+            imageDescription = R.string.lemon
+        ) {
+            if (countSqueezes < squeezes) {
+                task = LemonScreens.SQUEEZE
+                countSqueezes += 1
+            } else {
+                task = LemonScreens.DRINK
+            }
+        }
+        LemonScreens.DRINK -> LemonScreen(
+            textResource = R.string.drink_lemonade,
+            imageResource = R.drawable.lemon_drink,
+            imageDescription = R.string.lemonade_glass
+        ) {
+            task = LemonScreens.RESTART
+            resetSqueezes()
+        }
+        else -> LemonScreen(
+            textResource = R.string.start_again,
+            imageResource = R.drawable.lemon_restart,
+            imageDescription = R.string.empty_glass
+        ) {
+            task = LemonScreens.SELECT
+        }
+    }
+    /*val textResource: Int = when (task) {
         LemonScreens.SELECT -> R.string.select_lemon
         LemonScreens.SQUEEZE -> R.string.squeeze_lemon
         LemonScreens.DRINK -> R.string.drink_lemonade
@@ -65,31 +109,16 @@ fun LemonadeApp() {
         LemonScreens.SQUEEZE -> R.string.lemon
         LemonScreens.DRINK -> R.string.lemonade_glass
         else -> R.string.empty_glass
-    }
-    var squeezes: Int = (2..4).random()
-    var countSqueezes by remember {
-        mutableStateOf(0)
-    }
-    val resetSqueezes = {
-        squeezes = (2..4).random()
-        countSqueezes = 0
-    }
-/*    val onImageClicked = {
-        if (task == LemonScreens.SELECT) {
-            task = LemonScreens.SQUEEZE
-        } else if (task == LemonScreens.SQUEEZE) {
-            if (countSqueezes <= squeezes) {
-                task = LemonScreens.SQUEEZE
-                countSqueezes += 1
-            } else {
-                task = LemonScreens.DRINK
-            }
-        } else if (task == LemonScreens.DRINK) {
-            task = LemonScreens.RESTART
-        } else {
-            task = LemonScreens.SELECT
-        }
     }*/
+}
+
+@Composable
+fun LemonScreen(
+    textResource : Int,
+    imageResource : Int,
+    imageDescription : Int,
+    onImageClicked : () -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -110,27 +139,10 @@ fun LemonadeApp() {
                     color = Color(105, 205, 216),
                     shape = RoundedCornerShape(4.dp)
                 )
-                .clickable(onClick = {
-                    if (task == LemonScreens.SELECT) {
-                        task = LemonScreens.SQUEEZE
-                    } else if (task == LemonScreens.SQUEEZE) {
-                        if (countSqueezes < squeezes) {
-                            task = LemonScreens.SQUEEZE
-                            countSqueezes += 1
-                        } else {
-                            task = LemonScreens.DRINK
-                        }
-                    } else if (task == LemonScreens.DRINK) {
-                        task = LemonScreens.RESTART
-                        resetSqueezes()
-                    } else {
-                        task = LemonScreens.SELECT
-                    }
-                })
+                .clickable(onClick = onImageClicked)
         )
     }
 }
-
 
 @Preview(
     showBackground = true,
